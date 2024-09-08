ac81173ef5d1c121eb2ebbbed3be3f7c0ee9c400
th1nX <Flex0YT@yandex.ru>
2024-09-08 22:54:49 +0300

    Added auto .env creating

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
 @echo off
 IF EXIST .env (
-) ELSE (
-(echo API_ID = WRITE_YOUR_API_ID_INSTEAD && echo API_HASH = WRITE_YOUR_API_HASH_INSTEAD && echo PHONE_NUMBER = WRITE_ACCOUNT_PHONE_NUMBER_INSTEAD && echo ALLOWED_TGIDS = []) > .env
-)
 pip install -r requirements.txt
 main.py
 pause
+) ELSE (
+echo CREATED .env FILE. && echo PLEASE CHECK IT AND SET IT UP THEN.
+(echo API_ID = WRITE_YOUR_API_ID_INSTEAD && echo API_HASH = WRITE_YOUR_API_HASH_INSTEAD && echo PHONE_NUMBER = WRITE_ACCOUNT_PHONE_NUMBER_INSTEAD && echo ALLOWED_TGIDS = []) > .env
+)
