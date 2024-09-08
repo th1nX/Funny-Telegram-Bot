7c85ace031e69443696ad3f04ddc471746fbfb06
th1nX <Flex0YT@yandex.ru>
2024-09-08 20:54:52 +0300

    Some little changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
 from telethon import events
-from dbmodule import db
+from features import db
 from dotenv import load_dotenv
 from os import getenv
 
