f669d077774cc34b00efaac608012d0eed43d0e8
th1nX <Flex0YT@yandex.ru>
2024-09-08 23:32:28 +0300

    Added auto .env creating

--- features.py
+++ features.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-with sqlite3.connect('canals.db') as cursor:
+with sqlite3.connect('database.db') as cursor:
         command = """
         CREATE TABLE IF NOT EXISTS text (Id INTEGER PRIMARY KEY, 'Текст' TEXT);
         """
@@ -15,7 +15,7 @@ class db:
         self.count = count
 
     def insert_canal(number, id, name, status, count):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             INSERT OR REPLACE INTO tgcanals(id, 'ID Группы', 'Название группы', 'Статус', 'Кол-во участников')
             VALUES
@@ -24,35 +24,35 @@ class db:
             cursor.execute(command, (number, id, name, status, count))
 
     def del_canal_by_id(id):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             DELETE FROM tgcanals WHERE "ID Группы" = ?;
             """
             return cursor.execute(command, (id,))
 
     def del_canal_by_name(name):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             DELETE FROM tgcanals WHERE "Название группы" = ?;
             """
             return cursor.execute(command, (name,))
 
     def get_count_by_name(name):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             SELECT "Кол-во участников" FROM tgcanals WHERE "Название группы" = ?;
             """
             return cursor.execute(command, (name,)).fetchone()[0]
 
     def get_count_by_id(id):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             SELECT "Кол-во участников" FROM tgcanals WHERE "ID Группы" = ?;
             """
             return cursor.execute(command, (id,)).fetchone()[0]
 
     def get_all_id():
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             SELECT "ID Группы" FROM tgcanals;
             """
@@ -61,7 +61,7 @@ class db:
                 ids.append(int(i[0]))
             return ids
     def update_text(id, text):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             INSERT OR REPLACE INTO text(Id, 'Текст')
             VALUES
@@ -70,14 +70,14 @@ class db:
             cursor.execute(command, (id, text,))
 
     def get_text(id):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
             command = """
             SELECT "Текст" FROM text WHERE "Id" = ?;
             """
             return cursor.execute(command, (id,)).fetchone()[0]
 
     def create_db(self):
-        with sqlite3.connect('canals.db') as cursor:
+        with sqlite3.connect('database.db') as cursor:
                 command = """
                 CREATE TABLE IF NOT EXISTS tgcanals (Id INTEGER PRIMARY KEY, 'ID Группы' TEXT, 'Название группы' TEXT, 'Статус' TEXT, 'Кол-во участников' INTEGER);
                 """
