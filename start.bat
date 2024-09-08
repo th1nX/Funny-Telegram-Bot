3fbf8ade0f397069bd7491b97bb7e220f152414a
th1nX <Flex0YT@yandex.ru>
2024-09-08 22:48:00 +0300

    Nothing

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+(echo API_ID = WRITE_YOUR_API_ID_INSTEAD && echo API_HASH = WRITE_YOUR_API_HASH_INSTEAD && echo PHONE_NUMBER = WRITE_ACCOUNT_PHONE_NUMBER_INSTEAD && echo ALLOWED_TGIDS = []) > .env
 pip install -r requirements.txt
 main.py
 pause
